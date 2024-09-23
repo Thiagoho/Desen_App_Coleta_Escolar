e05185f1b191c2abccace8124fe9805ea2ab1814
Thiago H O S <thiago2006_sales@hotmail.com>
2024-09-23 05:26:26 -0300

    Atualizando o models commit

---
+++ src/test/java/com/AppCEs/AppCEs/AppCEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppCEs.AppCEs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
